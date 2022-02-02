292d17b430acae4560628ec981fca16579eefb11
SheebaHashmi <sheebahashmi0@gmail.com>
2022-02-02 16:08:24 -0500

    Fix User router

--- api/user/user-router.js
+++ api/user/user-router.js
@@ -7,9 +7,9 @@ const router = express.Router()
 
 router.post('/register',checkCredentials,validateUser,async (req,res,next) => {
     try{
-        const {username,password} = req.body
+        const {username,password,role_name} = req.body
         const hash = bcrypt.hashSync(password,8)
-        const response = await User.insertUser({username,password:hash})
+        const response = await User.insertUser({username,password:hash,role_name})
         res.status(201).json(response)
     }
     catch(err){
